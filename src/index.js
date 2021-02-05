81bce612328a0049a26a3eb40c2bde30f25d9e39
Intan Puspitasari <intanpusss@gmail.com>
2021-02-05 11:06:26 +0700

    remove unused css and add preload content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {ApolloClient, HttpLink, InMemoryCache, ApolloProvider} from "@apollo/client";
